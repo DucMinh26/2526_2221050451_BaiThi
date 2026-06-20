54aa72decef672fdc5752b7a1083255f8849165f
2221050451-NghiemDucMinh <minh2632004@gmail.com>
2026-06-20 16:13:54 +0700

    Cau 6

--- MVC/Models/Entities/NhanVien.cs
+++ MVC/Models/Entities/NhanVien.cs
@@ -5,7 +5,10 @@ namespace MVC.Models.Entities
     public class NhanVien
     {
         [Key]
+        [Required]
         public string MaNhanVien { get; set; }
+        [Required]
+        [MinLength(3)]
         public string? TenNhanVien { get; set; }
         public string MaPhongBan { get; set; }
         public PhongBan? PhongBan { get; set; }
